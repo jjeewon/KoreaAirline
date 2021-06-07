e6e33b63fc576d4f51f3789e3518b2374ffbd54c
jjeewon <wldnjs594@gmail.com>
2021-06-07 18:05:59 +0900

    애플리케이션 모듈에 App, Context 의존성 주입

--- app/src/main/java/com/gomdolstudio/travelkorea/di/AppModule.java
+++ app/src/main/java/com/gomdolstudio/travelkorea/di/AppModule.java
@@ -1,7 +1,28 @@
 package com.gomdolstudio.travelkorea.di;
 
+import android.app.Application;
+import android.content.Context;
+
+import com.gomdolstudio.travelkorea.App;
+
+import javax.inject.Singleton;
+
 import dagger.Module;
+import dagger.Provides;
 
 @Module
 public class AppModule {
+
+    @Provides
+    @Singleton
+    Application provideApp(App app){
+        return app;
+    }
+
+    @Provides
+    @Singleton
+    @ApplicationContext
+    Context provideContext(App app){
+        return app;
+    }
 }
