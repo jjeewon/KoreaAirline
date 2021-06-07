ffc75a56fd3daabcfbdb25e743cafe0b2754964c
jjeewon <wldnjs594@gmail.com>
2021-06-07 17:46:48 +0900

    App 생성
    DaggerApplication 상속

---
+++ app/src/main/java/com/gomdolstudio/travelkorea/App.java
@@ -0,0 +1,11 @@
+package com.gomdolstudio.travelkorea;
+
+import dagger.android.AndroidInjector;
+import dagger.android.DaggerApplication;
+
+public class App extends DaggerApplication {
+    @Override
+    protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
+        return null;
+    }
+}
