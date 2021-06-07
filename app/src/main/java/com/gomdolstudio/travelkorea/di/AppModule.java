c5af7706ce086909d6ed9c93df9abd809d83a92b
jjeewon <wldnjs594@gmail.com>
2021-06-07 18:16:13 +0900

    앱모듈에 errorEvent SingleLiveEvent 추가

--- app/src/main/java/com/gomdolstudio/travelkorea/di/AppModule.java
+++ app/src/main/java/com/gomdolstudio/travelkorea/di/AppModule.java
@@ -4,7 +4,9 @@ import android.app.Application;
 import android.content.Context;
 
 import com.gomdolstudio.travelkorea.App;
+import com.gomdolstudio.travelkorea.util.SingleLiveEvent;
 
+import javax.inject.Named;
 import javax.inject.Singleton;
 
 import dagger.Module;
@@ -24,5 +26,12 @@ public class AppModule {
     @ApplicationContext
     Context provideContext(App app){
         return app;
+    }
+
+    @Singleton
+    @Provides
+    @Named("errorEvent")
+    SingleLiveEvent<Throwable> provideErrorEvent(){
+        return new SingleLiveEvent<>();
     }
 }
