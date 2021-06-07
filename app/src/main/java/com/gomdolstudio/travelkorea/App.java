401fb56beb9b0911573f8a4cca7edb00322cf174
jjeewon <wldnjs594@gmail.com>
2021-06-07 18:00:02 +0900

    App의 applicationInjector 설정

--- app/src/main/java/com/gomdolstudio/travelkorea/App.java
+++ app/src/main/java/com/gomdolstudio/travelkorea/App.java
@@ -1,11 +1,13 @@
 package com.gomdolstudio.travelkorea;
 
+import com.gomdolstudio.travelkorea.di.DaggerAppComponent;
+
 import dagger.android.AndroidInjector;
 import dagger.android.DaggerApplication;
 
 public class App extends DaggerApplication {
     @Override
     protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
-        return null;
+        return DaggerAppComponent.factory().create(this);
     }
 }
