638627669fe69ecfc376bd8ff457c58586c87d9d
jjeewon <wldnjs594@gmail.com>
2021-06-08 16:11:31 +0900

    ViewModel 모듈에 뷰모델 추가

--- app/src/main/java/com/gomdolstudio/travelkorea/di/ViewModelModule.java
+++ app/src/main/java/com/gomdolstudio/travelkorea/di/ViewModelModule.java
@@ -1,9 +1,14 @@
 package com.gomdolstudio.travelkorea.di;
 
+import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
+
+import com.gomdolstudio.travelkorea.ui.flight.FlightViewModel;
+import com.gomdolstudio.travelkorea.ui.ticket.TicketViewModel;
 
 import dagger.Binds;
 import dagger.Module;
+import dagger.multibindings.IntoMap;
 
 // ViewModel과 관련된 내용을 오브젝트 그래프로 관리
 @Module
@@ -11,4 +16,14 @@ public abstract class ViewModelModule {
 
     @Binds
     abstract ViewModelProvider.Factory bindViewModelFactory(AppViewModelFactory factory);
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(FlightViewModel.class)
+    abstract ViewModel bindsFlightViewModel(FlightViewModel viewModel);
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(TicketViewModel.class)
+    abstract ViewModel bindsTicketViewModel(TicketViewModel viewModel);
 }
