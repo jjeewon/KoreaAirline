a089ac55fa9a4a42dee0f25ee9c80050268a7900
jjeewon <wldnjs594@gmail.com>
2021-06-08 16:01:51 +0900

    바인딩, 뷰모델팩토리 멤버인젝션

--- app/src/main/java/com/gomdolstudio/travelkorea/ui/flight/FlightFragment.java
+++ app/src/main/java/com/gomdolstudio/travelkorea/ui/flight/FlightFragment.java
@@ -1,4 +1,23 @@
 package com.gomdolstudio.travelkorea.ui.flight;
 
-public class FlightFragment {
+import com.gomdolstudio.travelkorea.databinding.FragmentSearchBinding;
+import com.gomdolstudio.travelkorea.di.AppViewModelFactory;
+
+import javax.inject.Inject;
+
+import dagger.android.support.DaggerFragment;
+
+public class FlightFragment extends DaggerFragment {
+
+    /**
+     * 오브젝트 그래프로부터 멤버 인젝션
+     */
+    @Inject
+    FragmentSearchBinding binding;
+    @Inject
+    AppViewModelFactory viewModelFactory;
+
+    FlightViewModel viewModel;
+
+    
 }
