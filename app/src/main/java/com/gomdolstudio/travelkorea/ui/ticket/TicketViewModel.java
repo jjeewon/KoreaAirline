52f7f1b0897fe0cee364bc6ec59c63f3f2ebff6f
jjeewon <wldnjs594@gmail.com>
2021-06-09 18:35:00 +0900

    Throwable 변수 선언

--- app/src/main/java/com/gomdolstudio/travelkorea/ui/ticket/TicketViewModel.java
+++ app/src/main/java/com/gomdolstudio/travelkorea/ui/ticket/TicketViewModel.java
@@ -7,23 +7,32 @@ import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.MutableLiveData;
 
 import com.gomdolstudio.travelkorea.data.entity.Flight;
+import com.gomdolstudio.travelkorea.util.SingleLiveEvent;
 
 import java.util.List;
 
 import javax.inject.Inject;
+import javax.inject.Named;
 
 import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import timber.log.Timber;
 
 public class TicketViewModel extends AndroidViewModel {
+
+    @NonNull
+    private final SingleLiveEvent<Throwable> errorEvent;
 
     private final MutableLiveData<Boolean> loading = new MutableLiveData<>(true);
 
     private final MutableLiveData<List<TicketItem>> liveTickets = new MutableLiveData<>();
     private final CompositeDisposable compositeDisposable = new CompositeDisposable();
-    
+
     @Inject
-    public TicketViewModel(@NonNull Application application) {
+    public TicketViewModel(@NonNull Application application, @Named("errorEvent") SingleLiveEvent<Throwable> errorEvent) {
         super(application);
+        Timber.d("PostViewModel created");
+        // 오브젝트 그래프로부터 생성자 주입
+        this.errorEvent = errorEvent;
     }
 
     public void loadTickets(){
