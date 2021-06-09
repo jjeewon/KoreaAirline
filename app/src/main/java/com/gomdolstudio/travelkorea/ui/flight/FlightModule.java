102503ec284a7c511cf2178798a4cb5b4d3b0d37
jjeewon <wldnjs594@gmail.com>
2021-06-09 14:51:15 +0900

    Flight 모듈에 NavController 메소드 추가
    멤버 인젝션

--- app/src/main/java/com/gomdolstudio/travelkorea/ui/flight/FlightModule.java
+++ app/src/main/java/com/gomdolstudio/travelkorea/ui/flight/FlightModule.java
@@ -3,6 +3,9 @@ package com.gomdolstudio.travelkorea.ui.flight;
 import android.content.Context;
 import android.view.LayoutInflater;
 
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+
 import com.gomdolstudio.travelkorea.databinding.FragmentSearchBinding;
 import com.gomdolstudio.travelkorea.di.ApplicationContext;
 import com.gomdolstudio.travelkorea.di.FragmentScope;
@@ -17,5 +20,12 @@ public class FlightModule {
     @FragmentScope
     FragmentSearchBinding provideBinding(@ApplicationContext Context context){
         return FragmentSearchBinding.inflate(LayoutInflater.from(context),null,false);
+    }
+
+    // Navigation 컴포넌트에서 목적지 간 이동을 담당하는 NavController
+    @Provides
+    @FragmentScope
+    NavController provideNavController(FlightFragment fragment){
+        return NavHostFragment.findNavController(fragment);
     }
 }
