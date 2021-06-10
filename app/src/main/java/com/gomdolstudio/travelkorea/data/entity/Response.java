49a4e7ca2f26ddec604134c2c052241307486947
jjeewon <wldnjs594@gmail.com>
2021-06-10 12:46:02 +0900

    Retrofit 서비스의 Response 정의

---
+++ app/src/main/java/com/gomdolstudio/travelkorea/data/entity/Response.java
@@ -0,0 +1,14 @@
+package com.gomdolstudio.travelkorea.data.entity;
+
+public class Response {
+    private Header header;
+    private Body body;
+
+    public Header getHeader() {
+        return header;
+    }
+
+    public Body getBody() {
+        return body;
+    }
+}
