864a2a4640feb10d1004ed8e33e3e72fc75353d4
jjeewon <wldnjs594@gmail.com>
2021-06-10 20:28:40 +0900

    Retrofit 서비스(Search Service) 내용 수정
    - @GET 내용 수정
    - 반환형 수정(TicketResponse)
      ** '서비스 메소드' 반환형이랑 뷰모델 compositeDisposable의 subscribe 'onSuccess' 자료형이 매칭됨!!

--- app/src/main/java/com/gomdolstudio/travelkorea/data/SearchService.java
+++ app/src/main/java/com/gomdolstudio/travelkorea/data/SearchService.java
@@ -1,6 +1,8 @@
 package com.gomdolstudio.travelkorea.data;
 
+import com.gomdolstudio.travelkorea.data.entity.Response;
 import com.gomdolstudio.travelkorea.data.entity.Ticket;
+import com.gomdolstudio.travelkorea.data.entity.TicketResponse;
 
 import java.util.List;
 
@@ -9,9 +11,9 @@ import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface SearchService {
-    @GET("getFlightOpratInfoList")
-    Single<List<Ticket>> getTicket(@Query("serviceKey") String serviceKey, @Query("numOfRows") String numOfRows, @Query("pageNo") String pageNo,
-                                   @Query("depAirportId") String depAirportId, @Query("arrAirportId") String arrAirportId,
-                                   @Query("depPlandTime") String depPlandTime, @Query("airlineId") String airlineId, @Query("_type") String _type);
+    @GET("openapi/service/DmstcFlightNvgInfoService/getFlightOpratInfoList")
+    Single<TicketResponse> getTicket(@Query("serviceKey") String serviceKey, @Query("numOfRows") String numOfRows, @Query("pageNo") String pageNo,
+                                     @Query("depAirportId") String depAirportId, @Query("arrAirportId") String arrAirportId,
+                                     @Query("depPlandTime") Long depPlandTime, @Query("_type") String _type);
 
 }
