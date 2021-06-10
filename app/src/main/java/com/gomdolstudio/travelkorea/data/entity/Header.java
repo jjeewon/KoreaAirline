a50f6d9cff7020e44fedfbc0433f6f31ee4e6e14
jjeewon <wldnjs594@gmail.com>
2021-06-10 12:09:15 +0900

    Retrofit 서비스 response의 Header 클래스 정의

---
+++ app/src/main/java/com/gomdolstudio/travelkorea/data/entity/Header.java
@@ -0,0 +1,14 @@
+package com.gomdolstudio.travelkorea.data.entity;
+
+public class Header {
+    private String resultCode;
+    private String resultMsg;
+
+    public String getResultCode() {
+        return resultCode;
+    }
+
+    public String getResultMsg() {
+        return resultMsg;
+    }
+}
